4397a40d94de94466fd2d74246d13580e7aec885
Arby Poetra <arbyharjani@gmail.com>
2026-06-08 14:59:12 +0700

    seccond change

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\Item;
+
+class MenuController extends Controller
+{
+    public function index(Request $request)
+    {   
+        $tableNumber = $request->query('meja');
+        if ($tableNumber) {
+            Session::put('tableNumber', $tableNumber);
+        }
+
+        $items = Item::where('is_active', 1)->orderBy('name', 'asc')->get();
+        
+        return view('customer.menu', compact('items', 'tableNumber'));
+    }
+
+    public function cart()
+    {
+        $cart = Session::get('cart');
+        return view('customer.cart', compact('cart'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        $id = $request->id;
+        $menu = Item::find($id);
+
+        if (!$menu) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Menu tidak ditemukan'
+            ]);
+        }
+
+        $cart = Session::get('cart');
+
+        if(isset($cart[$id])) {
+            $cart[$id]['qty'] += 1;
+        } else {
+            $cart[$id] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'image' => $menu->img,
+                'qty' => 1
+            ];
+        }
+
+        Session::put('cart', $cart);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil ditambahkan ke keranjang',
+            'cart' => $cart
+        ]);
+    }
+}
--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -16,7 +16,11 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 10000),
             'description' => $this->faker->text(),
-            'img' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement([
+                'https://images.unsplash.com/photo-1612927601601-6638404737ce',
+                'https://plus.unsplash.com/premium_photo-1668146927669-f2edf6e86f6f',
+                'https://images.unsplash.com/photo-1603133872878-684f208fb84b',
+            ]),
             'is_active' => $this->faker->boolean(),
         ];
     }
